01a162fd8a1f0a00028de89fb859d118327162c3
Renan <warreir@gmail.com>
2025-05-24 18:29:32 -0300

    feat: post

--- index.ts
+++ index.ts
@@ -2,16 +2,27 @@ import express, { type Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    console.log(req.query);
-    // http://localhost:9090/?num1=10&num2=20
-    res.send("Exemplo Query");
+app.use(express.json());
+
+app.get('/exercicio1', (req: Request, res: Response) => {
+    console.log("MetodoGet", req.query);
+    // http://localhost:9090/exercicio1/?num1=10&num2=20
+    const num1 = Number(req.query?.num1) || 0
+    const num2 = Number(req.query?.num2) || 0
+
+    const resultado = num1 + num2
+    res.send(resultado);
 });
 
-app.get('/api/:id/:parametros', (req: Request, res: Response) => {
-    // http://localhost:9090/api/id/parametros
-    console.log(req.params);
-    res.send("Exemplo Route");
+app.post('/exercicio1', (req: Request, res: Response) => {
+    console.log("MetodoPost", req.body);
+    // http://localhost:9090/exercicio1/
+    // Body - { "num1": 30, "num2": 50 }
+    const num1 = req.body?.num1 || 0
+    const num2 = req.body?.num2 || 0
+
+    const resultado = num1 + num2
+    res.send(resultado);
 });
 
 app.listen(9090, () => {
