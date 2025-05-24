c523ced0047e853223f4554dff32820b8fa0168a
Renan <warreir@gmail.com>
2025-05-24 16:50:41 -0300

    feat: hello

--- index.ts
+++ index.ts
@@ -1,25 +1,12 @@
-import http, { type IncomingMessage, ServerResponse } from 'http'
-import url from 'url'
+import express, { type Request, Response } from 'express';
 
-const server = http.createServer((request: IncomingMessage, response: ServerResponse) => {
-    const reqUrl = url.parse(request?.url || '', true)
-    
-    if(request.method === 'GET'){
-        console.log(reqUrl.query)
-        // pegar os parametros e realizar uma soma
+const app = express();
 
-        // http://localhost:9090?num1=10&num2=20
-        const num1 = Number(reqUrl.query?.num1);
-        const num2 = Number(reqUrl.query?.num2);
-        const resultado = num1 + num2;
+app.get('/', (req: Request, res: Response) => {
+    console.log("OIE");
+    res.send("Tesntado 123");
+});
 
-        response.writeHead(200, { 'content-type': 'text/plain; charset=utf-8' })
-        response.write("O resultado é "+ resultado);
-    }
-
-    response.end();
-})
-
-server.listen(9090, () => {
-    console.log("Iniciando servidor na porta 9090")
+app.listen(9090, () => {
+    console.log("Servidor rodando na porta 9090");
 })
