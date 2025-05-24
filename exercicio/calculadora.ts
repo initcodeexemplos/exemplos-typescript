d42e4211ab9259d8be919005f9a73d203502c653
Renan <warreir@gmail.com>
2025-05-24 18:56:44 -0300

    feat: finalizando calculadora

---
+++ exercicio/calculadora.ts
@@ -0,0 +1,15 @@
+export function somar(num1: number, num2: number) {
+    return num1+num2;
+}
+
+export function sub(num1: number, num2: number) {
+    return num1-num2;
+}
+
+export function mult(num1: number, num2: number) {
+    return num1*num2;
+}
+
+export function div(num1: number, num2: number) {
+    return num1/num2;
+}
--- index.ts
+++ index.ts
@@ -1,28 +1,36 @@
 import express, { type Request, Response } from 'express';
+import { div, mult, somar, sub } from './exercicio/calculadora';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/exercicio1', (req: Request, res: Response) => {
-    console.log("MetodoGet", req.query);
-    // http://localhost:9090/exercicio1/?num1=10&num2=20
-    const num1 = Number(req.query?.num1) || 0
-    const num2 = Number(req.query?.num2) || 0
-
-    const resultado = num1 + num2
-    res.send(resultado);
-});
-
-app.post('/exercicio1', (req: Request, res: Response) => {
-    console.log("MetodoPost", req.body);
-    // http://localhost:9090/exercicio1/
-    // Body - { "num1": 30, "num2": 50 }
+app.post('/somar', (req: Request, res: Response) => {
     const num1 = req.body?.num1 || 0
     const num2 = req.body?.num2 || 0
 
-    const resultado = num1 + num2
-    res.send(resultado);
+    res.send(somar(num1, num2));
+});
+
+app.post('/sub', (req: Request, res: Response) => {
+    const num1 = req.body?.num1 || 0
+    const num2 = req.body?.num2 || 0
+
+    res.send(sub(num1, num2));
+});
+
+app.post('/mult', (req: Request, res: Response) => {
+    const num1 = req.body?.num1 || 0
+    const num2 = req.body?.num2 || 0
+
+    res.send(mult(num1, num2));
+});
+
+app.post('/div', (req: Request, res: Response) => {
+    const num1 = req.body?.num1 || 0
+    const num2 = req.body?.num2 || 0
+
+    res.send(div(num1, num2));
 });
 
 app.listen(9090, () => {
