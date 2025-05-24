d4d73e49c54fae588f48d15c68a58d10b4ea1ad3
Renan <warreir@gmail.com>
2025-05-24 17:38:15 -0300

    feat: express query route params

--- index.ts
+++ index.ts
@@ -3,8 +3,15 @@ import express, { type Request, Response } from 'express';
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-    console.log("OIE");
-    res.send("Tesntado 123");
+    console.log(req.query);
+    // http://localhost:9090/?num1=10&num2=20
+    res.send("Exemplo Query");
+});
+
+app.get('/api/:id/:parametros', (req: Request, res: Response) => {
+    // http://localhost:9090/api/id/parametros
+    console.log(req.params);
+    res.send("Exemplo Route");
 });
 
 app.listen(9090, () => {
