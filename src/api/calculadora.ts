1b681e2283f22a119e08f345b939badfe706e004
Renan <warreir@gmail.com>
2025-05-26 20:44:51 -0300

    feat: try catch throw error

--- src/api/calculadora.ts
+++ src/api/calculadora.ts
@@ -1,35 +1,66 @@
 import { type Request, Response } from 'express'
 import { somar, sub, mult, div } from '../service/calculadora';
 
-export function apiSomar(req: Request, res: Response) {
-    console.log("entrou na pasta api")
-    const num1 = req.body?.num1 || 0
-    const num2 = req.body?.num2 || 0
-    const result = somar(num1, num2)
+export function apiSomar(req: Request, res: Response): void {
+    try {
+        const num1 = req.body?.num1 || 0
+        const num2 = req.body?.num2 || 0
+        const result = somar(num1, num2)
 
-    res.status(200).json({ result });
+        res.status(200).json({ result });
+    } catch (error) {
+        const err = error instanceof Error
+            ? error
+            : new Error(String(error))
+
+        res.status(500).json({ msg: err.message });
+    }
 }
 
 export function apiSub(req: Request, res: Response) {
-    const num1 = req.body?.num1 || 0
-    const num2 = req.body?.num2 || 0
-    const result = sub(num1, num2)
+    try{
+        const num1 = req.body?.num1 || 0
+        const num2 = req.body?.num2 || 0
+        const result = sub(num1, num2)
 
-    res.status(200).json({ result });
+        res.status(200).json({ result });
+    } catch (error) {
+        const err = error instanceof Error
+            ? error
+            : new Error(String(error))
+
+        res.status(500).json({ msg: err.message });
+    }
 }
 
 export function apiMult(req: Request, res: Response) {
-    const num1 = req.body?.num1 || 0
-    const num2 = req.body?.num2 || 0
-    const result = mult(num1, num2)
+    try {
+        const num1 = req.body?.num1 || 0
+        const num2 = req.body?.num2 || 0
+        const result = mult(num1, num2)
 
-    res.status(200).json({ result });
+        res.status(200).json({ result });
+    } catch (error) {
+        const err = error instanceof Error
+            ? error
+            : new Error(String(error))
+
+        res.status(500).json({ msg: err.message });
+    }
 }
 
 export function apiDiv(req: Request, res: Response) {
-    const num1 = req.body?.num1 || 0
-    const num2 = req.body?.num2 || 0
-    const result = div(num1, num2)
+    try{
+        const num1 = req.body?.num1 || 0
+        const num2 = req.body?.num2 || 0
+        const result = div(num1, num2)
 
-    res.status(200).json({ result });
+        res.status(200).json({ result });
+    } catch (error) {
+        const err = error instanceof Error
+            ? error
+            : new Error(String(error))
+
+        res.status(500).json({ msg: err.message });
+    }
 }
--- src/service/calculadora.ts
+++ src/service/calculadora.ts
@@ -1,16 +1,27 @@
 export function somar(num1: number, num2: number) {
-    console.log("entrou na pasta service")
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
     return num1+num2;
 }
 
 export function sub(num1: number, num2: number) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
     return num1-num2;
 }
 
 export function mult(num1: number, num2: number) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
     return num1*num2;
 }
 
 export function div(num1: number, num2: number) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números");
+    }
     return num1/num2;
 }
